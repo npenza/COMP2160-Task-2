b8ca91636523bcae2f2ce7a66f4840af4c673caa
shankie-afk <shashank.thigulla@students.mq.edu.au>
2021-10-21 00:27:50 +1100

    Added Camera Pivot

---
+++ COMP2160 Assignment Task 2/Assets/Scripts/CameraPivot.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPivot : MonoBehaviour
+{
+    public GameObject Car;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Car.transform.position;
+        
+    }
+}
