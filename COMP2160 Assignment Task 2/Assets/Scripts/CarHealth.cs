2341e65599e660c12a3f933a21ba89d162b8c8ab
Noah Penza <penzanoah@gmail.com>
2021-10-27 20:04:29 +1100

    Added CarHealth

---
+++ COMP2160 Assignment Task 2/Assets/Scripts/CarHealth.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarHealth : MonoBehaviour
+{
+
+    public int CurHealth;
+    private int curHealth = 100;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void onCollision()
+    {
+        curHealth = curHealth - 5;
+        Debug.Log(curHealth);
+    }
+
+    int UpdateHealth()
+    {
+        CurHealth = curHealth;
+        return CurHealth;
+    }
+}
